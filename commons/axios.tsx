f8878a48cc0181c1dfa646cdb6d9efa61843f36b
Kaleab Solomon <kaleabsolomon98@gmail.com>
2025-02-17 16:21:12 +0300

    Adding Successful message

--- commons/axios.tsx
+++ commons/axios.tsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: 'https://upd-party-backend.samesoft.app/api',
- //baseURL: 'http://localhost:3200/api',
+ // baseURL: 'https://upd-party-backend.samesoft.app/api',
+ baseURL: 'http://localhost:3200/api',
   headers: {
     'Content-Type': 'application/json',
   },
