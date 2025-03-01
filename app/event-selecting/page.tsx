37dc9af97c1d56f1de5336147852bd4f03d8506a
samesoft <samesoft.Co@gmail.com>
2025-03-01 21:00:46 +0300

    refactor: update header title from "Event Selecting" to "Upcoming Events"

--- app/event-selecting/page.tsx
+++ app/event-selecting/page.tsx
@@ -5,7 +5,7 @@ import EventSelectingPages from "@/components/event-selecting";
 export default function EventSelectingPage() {
     return (
         <div className="p-6">
-            <h1 className="text-2xl font-bold text-[#2E8B57] mb-6">Event Selecting</h1>
+            <h1 className="text-2xl font-bold text-[#2E8B57] mb-6 ">Upcoming Events</h1>
             <EventSelectingPages />
         </div>
     );
